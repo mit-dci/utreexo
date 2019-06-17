f2259941d87fad8e9e299034e2e0785969c5d40e
adiabat <rx@awsomnet.org>
2019-06-17 18:09:22 -0400

    there are still only children at the bottom, and that shouldn't happen. fix that before merging this into master

--- utreexo/pollard_test.go
+++ utreexo/pollard_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestRandPollard(t *testing.T) {
-	rand.Seed(222)
+	rand.Seed(1)
 	//	err := pollardMiscTest()
 	//	if err != nil {
 	//		t.Fatal(err)
@@ -19,7 +19,7 @@ func TestRandPollard(t *testing.T) {
 	//	}
 
 	//	for z := 0; z < 100; z++ {
-	err := pollardRandomRemember(1229)
+	err := pollardRandomRemember(29)
 	if err != nil {
 		t.Fatal(err)
 	}
