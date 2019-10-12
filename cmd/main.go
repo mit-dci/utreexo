912c2169d53d5217f04236111874b7f05ba502be
Gregory Markou <gregorymarkou@gmail.com>
2019-10-12 20:33:18 +0900

    remove default copyright

--- cmd/main.go
+++ cmd/main.go
@@ -19,10 +19,8 @@ var (
 // init initializes CLI
 func init() {
 	app.Action = utreexo
-	app.Copyright = "Copyright 2019 ChainSafe Systems Authors"
 	app.Name = "Utreexo"
 	app.Usage = "Official Utreexo command-line interface"
-	app.Author = "ChainSafe Systems 2019"
 	app.Version = "0.0.1"
 	app.Commands = []cli.Command{}
 	app.Flags = append(app.Flags, coreFlags...)
