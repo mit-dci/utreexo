82905623001dccb0ad674a98c2f07017e5968970
adiabat <rx@awsomnet.org>
2020-03-17 10:00:08 -0400

    can -> can't -> shouldn't be able to :)

--- utreexo/forest_test.go
+++ utreexo/forest_test.go
@@ -154,6 +154,7 @@ func TestDeleteNonExisting(t *testing.T) {
 	deletions := []uint64{0}
 	_, err := f.Modify(nil, deletions)
 	if err == nil {
-		t.Fatal(fmt.Errorf("can't delete non-existing leaf 0 from empty forest"))
+		t.Fatal(fmt.Errorf(
+			"shouldn't be able to delete non-existing leaf 0 from empty forest"))
 	}
 }
