0cf7adb998f3493ac517cfc2aabb85987bf35872
Niklas Gögge <n.goeggi@gmail.com>
2020-06-18 16:05:01 +0200

    test: also copy bitcoin-cli

--- test/install_bitcoind.sh
+++ test/install_bitcoind.sh
@@ -16,5 +16,6 @@ else
         wget https://bitcoin.org/bin/bitcoin-core-$BITCOIND_VERSION/bitcoin-$BITCOIND_VERSION-x86_64-linux-gnu.tar.gz && \
         tar xvfz bitcoin-$BITCOIND_VERSION-x86_64-linux-gnu.tar.gz && \
         sudo cp ./bitcoin-$BITCOIND_VERSION/bin/bitcoind /usr/local/bin/bitcoind && \
+        sudo cp ./bitcoin-$BITCOIND_VERSION/bin/bitcoin-cli /usr/local/bin/bitcoin-cli && \
         popd
 fi
