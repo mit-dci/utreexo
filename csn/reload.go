69e507626e743fd557faf28516a5a00eb8546ab2
Niklas Gögge <n.goeggi@gmail.com>
2020-07-21 23:17:38 +0200

    csn: Save utxos between runs

--- csn/reload.go
+++ csn/reload.go
@@ -5,19 +5,42 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/btcsuite/btcd/wire"
 	"github.com/mit-dci/utreexo/accumulator"
 	"github.com/mit-dci/utreexo/util"
 )
 
 // restorePollard restores the pollard from disk to memory.
 // If starting anew, it just returns a empty pollard.
-func restorePollard() (height int32, p accumulator.Pollard, err error) {
+func restorePollard() (height int32, p accumulator.Pollard,
+	utxos map[wire.OutPoint]util.LeafData, err error) {
 	// Restore Pollard
 	pollardFile, err := os.OpenFile(
 		util.PollardFilePath, os.O_RDWR, 0600)
 	if err != nil {
 		return
 	}
+
+	// restore utxos
+	var numUtxos uint32
+	err = binary.Read(pollardFile, binary.BigEndian, &numUtxos)
+	if err != nil {
+		return
+	}
+
+	utxos = make(map[wire.OutPoint]util.LeafData)
+	for ; numUtxos > 0; numUtxos-- {
+		var utxo util.LeafData
+		leafBytes := make([]byte, 32+32+4+4+8)
+		_, err = pollardFile.Read(leafBytes[:])
+		if err != nil {
+			return
+		}
+		util.LeafDataFromBytes(leafBytes)
+
+		utxos[utxo.Outpoint] = utxo
+	}
+
 	err = binary.Read(pollardFile, binary.BigEndian, &height)
 	if err != nil {
 		return
@@ -35,18 +58,32 @@ func restorePollard() (height int32, p accumulator.Pollard, err error) {
 // saveIBDsimData saves the state of ibdsim so that when the
 // user restarts, they'll be able to resume.
 // Saves height for ibdsim and pollard itself
-func saveIBDsimData(height int32, p accumulator.Pollard) error {
+func saveIBDsimData(csn *Csn) error {
 	polFile, err := os.OpenFile(
 		util.PollardFilePath, os.O_WRONLY, 0600)
 	if err != nil {
 		return err
 	}
-	// write to the heightfile
-	err = binary.Write(polFile, binary.BigEndian, height)
+
+	// save all found utxos
+	err = binary.Write(polFile, binary.BigEndian, uint32(len(csn.utxoStore)))
 	if err != nil {
 		return err
 	}
-	err = p.WritePollard(polFile)
+
+	for _, utxo := range csn.utxoStore {
+		_, err = polFile.Write(utxo.ToBytes()[:32+32+4+4+8])
+		if err != nil {
+			return err
+		}
+	}
+
+	// write to the heightfile
+	err = binary.Write(polFile, binary.BigEndian, csn.CurrentHeight)
+	if err != nil {
+		return err
+	}
+	err = csn.pollard.WritePollard(polFile)
 	if err != nil {
 		return err
 	}
