bd0360c243660a0ac20c78405c864509f16a0fff
Calvin Kim <calvin@kcalvinalvin.info>
2020-11-12 17:37:50 +0900

    accumulator/hasher.go: Faster hashrow
    Faster hashrow by getting rid of all the channel sends/receives

--- accumulator/hasher.go
+++ accumulator/hasher.go
@@ -6,32 +6,11 @@ type hashableNode struct {
 	position  uint64 // doesn't really need to be there, but convenient for debugging
 }
 
-type hashNpos struct {
-	result Hash
-	pos    uint64
-}
-
-func hashOne(l, r Hash, p uint64, hchan chan hashNpos) {
-	var hnp hashNpos
-	hnp.pos = p
-	hnp.result = parentHash(l, r)
-	hchan <- hnp
-}
-
 func (f *Forest) hashRow(dirtpositions []uint64) error {
-
-	hchan := make(chan hashNpos, 256) // probably don't need that big a buffer
-
 	for _, hp := range dirtpositions {
 		l := f.data.read(child(hp, f.rows))
 		r := f.data.read(child(hp, f.rows) | 1)
-		// fmt.Printf("hash pos %d l %x r %x\n", hp, l[:4], r[:4])
-		go hashOne(l, r, hp, hchan)
-	}
-
-	for remaining := len(dirtpositions); remaining > 0; remaining-- {
-		hnp := <-hchan
-		f.data.write(hnp.pos, hnp.result)
+		f.data.write(hp, parentHash(l, r))
 	}
 
 	return nil
