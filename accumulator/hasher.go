f8fd1c4bd5bb2fd78940f4e9db45dd8be12d1b39
eddogola <64967909+eddogola@users.noreply.github.com>
2021-11-08 08:30:24 -0500

    remove continous hashing from hashrow

--- accumulator/hasher.go
+++ accumulator/hasher.go
@@ -1,5 +1,9 @@
 package accumulator
 
+import (
+	"sync"
+)
+
 // hashableNode is the data needed to perform a hash
 type hashableNode struct {
 	sib, dest *polNode
@@ -9,32 +13,24 @@ type hashableNode struct {
 // hashRow calculates new hashes for all the positions passed in
 func (f *Forest) hashRow(dirtpositions []uint64) []uint64 {
 
-	// t := time.Now()
-
 	// make wg
-	// var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-	// wg.Add(len(dirtpositions))
+	wg.Add(len(dirtpositions))
 
-	// for _, hp := range dirtpositions {
+	for _, hp := range dirtpositions {
 
-	// 	go func() {
-	// 		// t1 := time.Now()
-	// 		l := f.data.read(child(hp, f.rows))
-	// 		r := f.data.read(child(hp, f.rows) | 1)
-	// 		// fmt.Println("Reads take ", time.Since(t)) // each read takes from 42-207ns
-	// 		f.data.write(hp, parentHash(l, r))
-	// 		// t2 := time.Now()
-	// 		// fmt.Println("Writes take", time.Since(t2)) // each write takes anything from 30-130ns
-	// 		wg.Done()
-	// 	}()
-	// }
+		go func() {
+			l := f.data.read(child(hp, f.rows))
+			r := f.data.read(child(hp, f.rows) | 1)
+			f.data.write(hp, parentHash(l, r))
+			wg.Done()
+		}()
+	}
 
-	// wg.Wait()
+	wg.Wait()
 
-	// fmt.Println("Total time taken by I/O", time.Since(t))
-
-	// return nil
+	return nil
 
 	// for _, hp := range dirtpositions {
 	// 	l := f.data.read(child(hp, f.rows))
@@ -42,16 +38,5 @@ func (f *Forest) hashRow(dirtpositions []uint64) []uint64 {
 	// 	f.data.write(hp, parentHash(l, r))
 	// }
 
-	return nil
-
-	// try
-	var nextRow []uint64
-	for _, dp := range dirtpositions {
-		if dp&1 == 1 {
-			l := f.data.read(child(dp, f.rows))
-			r := f.data.read(child(dp, f.rows) | 1)
-			pHash := parentHash(l, r)
-			nextRow = append(nextRow, )
-		}
-	}
+	// return nil
 }
