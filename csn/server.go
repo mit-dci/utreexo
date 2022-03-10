733fbe0dbe9cb9fd21d63457380ed2b346d79062
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-10 19:46:16 +0800

    Delete csn directory

--- csn/server.go
+++
@@ -1,9 +0,0 @@
-package csn
-
-import "github.com/mit-dci/utreexo/accumulator"
-
-// CsnHook is the main stateful struct for the Compact State Node.
-// It keeps track of what block its on and what transactions it's looking for
-type CompactState struct {
-	pollard accumulator.Pollard
-}
